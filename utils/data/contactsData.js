3e2ed5ef25aae7d20d0bab8bb785c5cacf8e68ae
fGHG99 <azkalearn10@gmail.com>
2024-09-07 13:33:01 +0700

    delete icon

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,9 +3,7 @@ export const contactsData = {
     phone: '+62-813-2119-666',
     address: 'Bandung, Kota Bunga dimana kisah kita dimulai ',
     github: 'https://github.com/fGHG99',
-    facebook: '',
-    linkedIn: '',
-    twitter: '',
-    stackOverflow: '',
+    discord: 'https://discordapp.com/users/543353597056778240',
+    linkedIn: 'https://www.linkedin.com/in/azka-dafin-72703b204/',
     devUsername: "fGHG99"
 }
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -7,12 +7,7 @@ export const personalData = {
   phone: '+62 813-2119-666',
   address: 'Bandung, Kota bunga dimana kisah kita dimulai ',
   github: 'https://github.com/fGHG99',
-  facebook: '',
-  linkedIn: '',
-  twitter: '',
-  stackOverflow: '',
-  leetcode: "",
-  devUsername: "",
+  linkedIn: 'https://www.linkedin.com/in/azka-dafin-72703b204/',
   discord: 'https://discordapp.com/users/543353597056778240',  
   resume: "https://drive.google.com/drive/folders/1r7OMAy7IRTnOMVxwlTeVb7n75LE50gR-?usp=drive_link"
 }
